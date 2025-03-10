1e9ef41bb59c5d702ac4cb7a06ed2e4e57d0715e
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-10 10:18:44 -0600

    pushing!

--- src/Adair.java
+++ src/Adair.java
@@ -4,8 +4,12 @@ public class Adair
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				
+				
+			}
+		public void printSomething()
+			{
+			System.out.println("Adair!");
 			}
 
 	}
